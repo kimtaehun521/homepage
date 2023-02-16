80f2ec5b689c2b3163c5a249c11ac069474d5fdd
kimtaehun521 <123814543+kimtaehun521@users.noreply.github.com>
2023-02-16 17:38:55 +0900

    Add files via upload

---
+++ practice/js/main.js
@@ -0,0 +1,93 @@
+
+(function() {
+  "use strict";
+
+const $text = document.querySelector(".text");
+const letters = [
+  "Java",
+  "HTML",
+  "CSS", 
+  "JavaScript"
+];
+
+const speed = 100;
+let i = 0;
+
+const typing = async () => {  
+  const letter = letters[i].split("");
+  
+  while (letter.length) {
+    await wait(speed);
+    $text.innerHTML += letter.shift(); 
+  }
+  
+  await wait(800);
+  remove();
+}
+
+const remove = async () => {
+  const letter = letters[i].split("");
+  
+  while (letter.length) {
+    await wait(speed);
+    
+    letter.pop();
+    $text.innerHTML = letter.join(""); 
+  }
+  
+  
+  i = !letters[i+1] ? 0 : i + 1;
+  typing();
+}
+
+function wait(ms) {
+  return new Promise(res => setTimeout(res, ms))
+}
+
+setTimeout(typing, 1500);
+
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+})()
